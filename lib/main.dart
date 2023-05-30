e9d418c2d958588afe3921fa0ef5a116d4236dc5
HangHH <hhuuhang@gmail.com>
2023-05-30 12:31:56 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bài tập flutter widget 01',
+      title: 'Bài tập flutter widget',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
